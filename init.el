9102a9b54dc313742decbb076abb8bbe8421108d
kawahara <ibura11.2@gmail.com>
2020-03-22 09:30:27 +0900

    delete unused lines

--- init.el
+++ init.el
@@ -113,16 +113,3 @@
        '(("\\.v$" . verilog-mode))
        '(("\\.sv$" . verilog-mode))
        auto-mode-alist))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company material-theme better-defaults))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
